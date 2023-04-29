13eddecce161e9aa8243e4887a672d42759f2d1f
7wc98#14 <2923616405@qq.com>
2023-04-29 20:16:40 +0800

    注意力机制代码up

--- Transformer/simple_test.py
+++ Transformer/simple_test.py
@@ -9,16 +9,9 @@ import copy
 
 from MyEmbedding import MyEmbedding
 from PositionalEncoding import PositionalEncoding
+from utils import subsequent_mask, attention
 
-if __name__ == "__main__":
-    # embedding = nn.Embedding(10, 3)
-    # input1 = torch.LongTensor([[1, 2, 4, 5], [4, 3, 2, 9]])
-    # print(embedding(input1))
-
-    # embedding = nn.Embedding(10, 3, padding_idx=0)
-    # input1 = torch.LongTensor([0, 2, 0, 5])
-    # print(embedding(input1))
-
+def use() :
     d_model = 512
     vocab = 1000
     x = Variable(torch.LongTensor([[100, 2, 421, 508], [491, 998, 1, 221]]))
@@ -29,8 +22,43 @@ if __name__ == "__main__":
     y = embr
     pe = PositionalEncoding(d_model, dropout, max_len)
     pe_result = pe(y)
-    print(pe_result)
-    print(pe_result.shape)
+    query = key = value = pe_result
+    mask = Variable(torch.zeros(2, 4, 4))
+    attn, p_attn = attention(query=query, key=key, value=value, mask=mask)
+    
+
+if __name__ == "__main__":
+    use()
+    # sm = subsequent_mask(5)
+    # print("sm: ", sm)
+    # plt.figure(figsize=(5, 5))
+    # plt.imshow(sm[0])
+    # plt.show()
+    # 利用三角矩阵进行掩码
+    # matrix = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+    # tri_matrix = np.triu(matrix, k=-1)
+    # print(tri_matrix)
+    # print(np.triu(matrix, 0))
+
+    # embedding = nn.Embedding(10, 3)
+    # input1 = torch.LongTensor([[1, 2, 4, 5], [4, 3, 2, 9]])
+    # print(embedding(input1))
+
+    # embedding = nn.Embedding(10, 3, padding_idx=0)
+    # input1 = torch.LongTensor([0, 2, 0, 5])
+    # print(embedding(input1))
+
+    # # 设置画布
+    # plt.figure(figsize=(15, 5))
+
+    # # 实例化 PositionalEncoding 类对象，词嵌入维度 20，置零比例未 0
+    # pe2 = PositionalEncoding(20, 0)
+
+    # t = pe2(Variable(torch.zeros(1, 100, 20)))
+    # plt.plot(np.arange(100), t[0, :, 4:8].data.numpy())
+    # plt.legend(["dim %d"%p for p in [4, 5, 6, 7]])
+    # plt.show()
+    
 
 
     # m = nn.Dropout(p=0.2)
@@ -41,3 +69,8 @@ if __name__ == "__main__":
     # x = torch.tensor([1, 2, 3, 4])
     # print(torch.unsqueeze(x,1))
     # print(torch.unsqueeze(x, 1))
+
+    # x = Variable(torch.randn(5, 5))
+    # mask = Variable(torch.zeros(5, 5))
+    # y = x.masked_fill(mask==0, -1e9)
+    # print(y)
---
+++ Transformer/utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+import torch
+import math
+import torch.nn.functional as F
+
+def subsequent_mask(size):
+    # size 代表掩码张量后两个维度，形成一个方阵
+    attn_shape = (1, size, size)
+
+    # 使用 np.ones() 先构建一个全 1 的张量，然后利用 np.triu() 形成上三角矩阵
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    #使得三角矩阵进行反转
+    return torch.from_numpy(1 - subsequent_mask)
+
+def attention(query, key, value, mask=None, dropout=None):
+    # query, key, value: 代表注意力的三个输入张量
+    # mask：掩码张量
+    # dropout：传入的 Dropout 实例化对象
+    # 首先将 query 的最后一个维度提取出来，代表词嵌入的维度
+    d_k = query.size(-1)
+
+    # 按照注意力计算公式，将 query 和 key 的转置矩阵进行矩阵乘法，然后除以缩放系数
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    
+    # 判断是否使用掩码张量
+    if mask is not None:
+        # 利用 masked_fill 方法，将掩码张量和 0 进行比较，若等于0，则替换成一个非常小数值
+        scores = scores.masked_fill(mask==0, -1e9)
+    
+    # 对 scores 的最后一个维度进行 softmax 操作
+    p_pattn = F.softmax(scores, dim=-1)
+
+    # 判断是否使用 dropout
+    if dropout is not None:
+        p_pattn = dropout(p_pattn)
+    
+    # 最后一步完成 p_attn 和 value 张量的乘法，并返回 query 的注意力表示
+    return torch.matmul(p_pattn, value), p_pattn
